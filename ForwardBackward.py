de8346e943b4adfd0fd4b385a5560042abd5fadb
Jørgen Sund <jorgess@stud.ntnu.no>
2018-02-28 23:30:55 +0100

    Finished

--- ForwardBackward.py
+++ ForwardBackward.py
@@ -5,39 +5,56 @@ import hmm
 def forward(hmm,dist,emissions):
     dists = [dist]
     em = []
-    em.append(np.dot(hmm.getTransitionProb(),np.diagflat(hmm.getEmissionProb(0))))
+    em.append(np.dot(hmm.getTransitionProb(),np.diagflat(hmm.getEmissionProb(0)))) #Precalculate these
     em.append(np.dot(hmm.getTransitionProb(),np.diagflat(hmm.getEmissionProb(1))))
 
     for emission in emissions:
-        dist = forwardStep(hmm,dist,em[emission])
+        dist = forwardStep(dist,em[emission])
         dists.append(dist)
     return dists
 
-def normalize(a):
-    theSum = sum(a)
-    for i in range(len(a)):
-        a[i] /= theSum
 
-def forwardStep(hmm,dist,em):
+def normalize(a):
+     theSum = sum(a)
+     for i in range(len(a)):
+         a[i] /= theSum
+     return a
+
+
+def forwardStep(dist,em):
     dist = np.dot(dist,em)
     normalize(dist)
     return dist
 
 
+def backward(hmm,emissions):
+    dist = np.array([0.5, 0.5])
+    dists = [dist]
+    for emission in emissions:
+        dist = backwardStep(hmm,dist,emission)
+        dists.append(dist)
+    return dists
 
 
+def backwardStep(hmm,dist,em):
+    return normalize(np.dot(hmm.getTransitionProb(), np.dot(np.diagflat(hmm.getEmissionProb(em)), dist.T)).T)
 
 
-def backward():
-    pass
+def forwardBackward(hmm, dist, emissions):
+    forwardDists = forward(hmm, dist, emissions)
+    backwardDists = backward(hmm, emissions)
 
+    return normalize(np.multiply(forwardDists, backwardDists))
 
 emissionProbs = np.array([[0.9,0.1],[0.2,0.8]])
 transitionProbs = np.array([[0.7,0.3],[0.3,0.7]])
 hmm = hmm.HiddenMarkov(transitionProbs,emissionProbs)
 initialDist = [0.5,0.5]
-emissions = [0,0,1,0,0]
+#emissions = [0,0,1,0,0]
+emissions = [0,1,0,1]
 
 if __name__ == "__main__":
     print(forward(hmm,initialDist,emissions))
+    print(backward(hmm,emissions))
+    print(forwardBackward(hmm,initialDist,emissions))
 
